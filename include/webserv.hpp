fe9d6e9af16894a0089cc369d029c05e046bf845
asqel <axel.lleres@gmail.com>
2025-12-09 15:26:52 +0100

    kk

--- include/webserv.hpp
+++ include/webserv.hpp
@@ -1,12 +1,25 @@
-#ifndef CONF_HPP
-#define CONF_HPP
+#ifndef WEBSERV_HPP
+#define WEBSERV_HPP
 
 #include <iostream>
 #include <map>
 #include <vector>
 #include <string>
 
+class Client {
+	private:
+		int fd;
+
+	public:
+		std::string data;
+}
+
 class Interface {
+	private:
+		int server_fd;
+		std::vector<Client> clients;
+		std::vector<int> incomming_fds;
+	
 	public:
 		std::string host;
 		int port;
@@ -24,6 +37,19 @@ class Interface {
 
 		std::map<std::string, std::string> cgi;
 		std::string storage_path;
+
 };
+
+class WebServ {
+	private:
+		std::vector<Interface> interfaces;
+		struct pollfd *fds;
+		int fds_len;
+
+	public:
+			
+
+
+}
 
 #endif
